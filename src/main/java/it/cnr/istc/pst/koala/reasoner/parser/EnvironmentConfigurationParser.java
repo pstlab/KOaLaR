889dff1af22c69f4bda978de0273503cdff2571e
alessandro <umbrico.alessandro@gmail.com>
2017-10-02 17:25:02 +0200

    Project setup

---
+++ src/main/java/it/cnr/istc/pst/koala/reasoner/parser/EnvironmentConfigurationParser.java
@@ -0,0 +1,19 @@
+package it.cnr.istc.pst.koala.reasoner.parser;
+
+/**
+ * 
+ * @author alessandro
+ *
+ */
+public class EnvironmentConfigurationParser 
+{
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
--- src/main/java/it/cnr/istc/pst/koala/reasoner/parser/XMLParser.java
+++
@@ -1,5 +0,0 @@
-package it.cnr.istc.pst.koala.reasoner.parser;
-
-public class XMLParser {
-
-}
